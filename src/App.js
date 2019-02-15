a04bb92fa136018efe41f3c61d7e35236f82042d
Leonam Souza <leonamadm@hotmail.com>
2019-02-15 13:19:37 -0200

    component will unmount

--- src/App.js
+++ src/App.js
@@ -1,28 +1,65 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Enthused } from './Enthused';
 
-class App extends Component {
+export class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      enthused: false,
+      text: ''
+    };
+
+    this.toggleEnthusiasm = this.toggleEnthusiasm.bind(this);
+    this.addText = this.addText.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  toggleEnthusiasm() {
+    this.setState({
+      enthused: !this.state.enthused
+    });
+  }
+
+  setText(text) {
+    this.setState({ text: text });
+  }
+
+  addText(newText) {
+    let text = this.state.text + newText;
+    this.setState({ text: text });
+  }
+
+  handleChange(e) {
+    this.setText(e.target.value);
+  }
+
   render() {
+    let button;
+    if (this.state.enthused) {
+      button = (
+        <Enthused toggle={this.toggleEnthusiasm} addText={this.addText} />
+      );
+    } else {
+      button = (
+        <button onClick={this.toggleEnthusiasm}>
+          Add Enthusiasm!
+        </button>
+      );
+    }
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <h1>Auto-Enthusiasm</h1>
+        <textarea rows="7" cols="40" value={this.state.text} 
+          onChange={this.handleChange}>
+        </textarea>
+        {button}
+        <h2>{this.state.text}</h2>
       </div>
     );
   }
 }
 
 export default App;
+
